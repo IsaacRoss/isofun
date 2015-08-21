00512cfa228ee63662fa3df6040aadcf1067cfda
Isaac Ross <isaacross@Isaacs-MBP.fios-router.home>
2015-08-20 20:20:48 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    LiveServer = require('gulp-live-server'),
+    browserSync = require('browser-sync'),
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('vinyl-source-stream');
+
+
+gulp.task('live-server', function(){
+    var server = new LiveServer('server/main.js');
+    server.start();
+});
+
+gulp.task('bundle', function(){
+    return browserify({
+        entries: 'app/main.jsx',
+        debug: true
+    })
+        .transform(reactify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./.tmp'));
+});
+
+gulp.task('serve', ['bundle','live-server'], function(){
+    browserSync.init(null, {
+        proxy: 'http://localhost:3000',
+        port: 9001
+    })
+});
+
